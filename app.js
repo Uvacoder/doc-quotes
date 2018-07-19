ef9705c3bd023ba643575567e86998494392c16d
Onur <onur.suyalcinkaya@sistas.com.tr>
2018-07-19 11:40:55 +0300

    update: font, UI

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ new Vue({
     data: {
         quotes: [],
         quote: {},
-        colors: ["#fff", "#3B5A69", "#FF5A5F", "#00E6B5", "FA9A94", "4381F6"],
+        colors: ["#fff", "#3B5A69", "#FF5A5F", "#00E6B5", "#FA9A94", "#4381F6"],
         color: ""
     },
     created() {
         this.$http.get('./quotes.json').then(response => {
             this.quotes = response.body; // or response.data
-            this.quote = this.getRandom();
-            this.color = this.randomColor();
+            this.quote = this.getRandomQuote();
+            this.color = this.getRandomColor();
         }, response => {
             alert("Sorry! Could not get quotes.");
         });
@@ -18,18 +18,22 @@ new Vue({
     methods: {
         changeQuote() {
             let tempQuote = {};
-            do { tempQuote = this.getRandom() } while(tempQuote.text === this.quote.text);
+            do { tempQuote = this.getRandomQuote() } while(tempQuote.text === this.quote.text);
             this.quote = tempQuote;
             console.log('Quote: ' + this.quote.text);
-            this.color = this.randomColor();
+            this.changeColor();
         },
-        getRandom() {
+        getRandomQuote() {
             return this.quotes[Math.floor(Math.random() * this.quotes.length)];
         },
-        randomColor() {
-            let randomColor = this.colors[Math.floor(Math.random() * this.colors.length)];
-            console.log('Color: ' + randomColor);
-            return randomColor;
+        changeColor() {
+            let tempColor = "";
+            do { tempColor = this.getRandomColor() } while(tempColor === this.color);
+            this.color = tempColor;
+            console.log('Color: ' + tempColor);
+        },
+        getRandomColor() {
+            return this.colors[Math.floor(Math.random() * this.colors.length)];
         }
     }
 });
